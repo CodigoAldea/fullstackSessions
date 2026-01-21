d7a0a8bb3f60c12848409514f584d61273601920
Codigo Aldea <dmdndrd@gmail.com>
2026-01-21 19:23:52 +0530

    update

--- project/exptracker/views.py
+++ project/exptracker/views.py
@@ -3,7 +3,7 @@ from exptracker.models import expense
 
 def home(request):
     data = expense.objects.all()
-    #total = expense.aggregate(total = Sum('amount'))[total] # we are calculating the total expense 
+   
     total = sum(i.amount for i in data)
     if request.method == 'POST':
         category = request.POST.get('category')
