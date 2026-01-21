217ba30f852f910f676a2ea3b06d1c00499a7523
Codigo Aldea <dmdndrd@gmail.com>
2026-01-21 18:25:13 +0530

    update for the discord link

--- project/exptracker/models.py
+++ project/exptracker/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+
+class expense(models.Model):
+    
+    CATEGORY_CHOICES= [
+        # value , display value
+        ('Food', 'Food'),
+        ('Travel', 'Travel'),
+        ('Others', 'Others')
+    ]
+    
+    category = models.CharField(max_length=100, choices= CATEGORY_CHOICES)
+    description = models.CharField(max_length=500)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date= models.DateField(auto_now_add= True)
