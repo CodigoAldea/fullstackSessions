0ec99bd035d11d3c592edf5055b53452f7cc9078
Codigo Aldea <dmdndrd@gmail.com>
2026-01-21 19:01:28 +0530

    update

--- project/exptracker/views.py
+++ project/exptracker/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from exptracker.models import expense
 
-# Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    data = expense.objects.all()
+    #total = expense.aggregate(total = Sum('amount'))[total] # we are calculating the total expense 
+    total = sum(i.amount for i in data)
+    if request.method == 'POST':
+        category = request.POST.get('category')
+        description = request.POST.get('description')
+        amount = request.POST.get('amount')
+        
+        expense.objects.create(
+            category= category,
+            description = description,
+            amount= amount
+        )
+        
+    return render(request, 'home.html', {'data' : data, 'total': total})
